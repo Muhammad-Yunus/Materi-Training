3148fb267c60410333178d6afd2f06ea17d85279
Muhammad Yunus <34908065+Muhammad-Yunus@users.noreply.github.com>
2021-01-19 17:54:03 +0700

    update

--- F. Mobile App - Kotlin/2. Dice Roller/app/src/main/java/com/example/a2diceroller/MainActivity.kt
+++ F. Mobile App - Kotlin/2. Dice Roller/app/src/main/java/com/example/a2diceroller/MainActivity.kt
@@ -2,11 +2,40 @@ package com.example.a2diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+    private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        // Update the screen with the dice roll
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
